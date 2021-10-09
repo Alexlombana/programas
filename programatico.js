12d178eaca7770fa4f22f1dfe61c225fd00a7b91
Alexander Lombana <alexlombanac@hotmail.com>
2021-10-09 16:34:52 -0500

    segunda Actualización

--- programatico.js
+++ programatico.js
@@ -1,14 +1,14 @@
 /* Programa que dado un numero retorne true si es un numero par o retorne false si es un numero impar y para los multiplos de 5 retorne go*/
-var numero=10;
+var numero=15;
 
-if (numero%2==0){
+if (numero%2==0 && numero % 5!= 0){
     console.log(`True, ${numero} es un numero par`);
 }
-else {
+else if (numero%2!=0 && numero % 5!= 0) {
     console.log(`false, ${numero} es un numero impar`);
 }
 if ( numero > 0 && numero % 5 == 0 ) {
-    console.log(`GO ${numero},es múltiplo de 5` )
+    console.log("GO" )
 
  }
  
