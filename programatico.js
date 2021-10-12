1d94cab7093b524f99f3e413c667f4967e04cca4
Alexander Lombana <alexlombanac@hotmail.com>
2021-10-11 22:47:44 -0500

    primer programa con función

--- programatico.js
+++ programatico.js
@@ -1,15 +1,20 @@
 /* Programa que dado un numero retorne true si es un numero par o retorne false si es un numero impar y para los multiplos de 5 retorne go*/
-var numero=15;
 
+function matematica(numero) {
 if (numero%2==0 && numero % 5!= 0){
-    console.log(`True, ${numero} es un numero par`);
+    console.log(`True`);
 }
-else if (numero%2!=0 && numero % 5!= 0) {
-    console.log(`false, ${numero} es un numero impar`);
+else if(numero % 5!= 0) {
+    console.log(`false`);
 }
 if ( numero > 0 && numero % 5 == 0 ) {
     console.log("GO" )
 
  }
+}
+console.log(matematica(7))
+
+
  
 
+ 
